ee46d4120080813d0c1fbeccc411783f2c09cb13
Paige Trask <paigetrask@gmail.com>
2022-06-25 15:42:58 -0700

    Updates

---
+++ precache-manifest.9e215a4fee670a04506d1969c692fdc6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "197acaa48572304ceb82",
+    "url": "/digital-business-card/static/css/main.02bfb9bf.chunk.css"
+  },
+  {
+    "revision": "197acaa48572304ceb82",
+    "url": "/digital-business-card/static/js/main.197acaa4.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/digital-business-card/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "7d89464741f2193926da",
+    "url": "/digital-business-card/static/js/2.7d894647.chunk.js"
+  },
+  {
+    "revision": "78672dd0ea55520cccbb",
+    "url": "/digital-business-card/static/js/runtime~main.78672dd0.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/digital-business-card/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "6d0cda14645459d7df3cbaa19f0cc9fb",
+    "url": "/digital-business-card/static/media/PaigeSquare.6d0cda14.jpeg"
+  },
+  {
+    "revision": "58aa3222502e9b77ee793f52c0fe99c9",
+    "url": "/digital-business-card/index.html"
+  }
+];
